dca2fa34783d833b86dbf7a1f29b4d93e624440b
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-11-27 17:14:54 +0800

    Just the pusedocode for the first program

---
+++ personal_information_collector/personal_information_collector.py
@@ -0,0 +1,1 @@
+#Create a program that ask user for personal information. Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. The program should ask user if want to input another person or exit.
