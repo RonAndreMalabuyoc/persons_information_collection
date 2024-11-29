316e3643bf237d3f23851ba4042080d4ca540a22
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-11-29 18:22:54 +0800

    clean up the program

--- personal_information_collector/personal_information_collector.py
+++ personal_information_collector/personal_information_collector.py
@@ -1,4 +1,3 @@
-#Create a program that ask user for personal information. Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. The program should ask user if want to input another person or exit.
 def collect_information():
     return {
         "Full name": input("Please put in your full name: ").strip(),
